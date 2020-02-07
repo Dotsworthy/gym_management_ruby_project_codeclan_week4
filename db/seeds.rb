b66eea5c0f658a77e87ea0df9694d22b3273487f
Dotsworthy <dotsworthy@gmail.com>
2020-02-07 22:14:45 +0000

    added create functionality and fixed sql_runner.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,19 @@ require_relative( "../models/session.rb" )
 require_relative( "../models/booking.rb" )
 require("pry-byebug")
 
+Member.delete_all()
+
 member1 = Member.new({
   "first_name" => "Dwayne",
   "last_name" => "Johnson"
   })
+member1.save()
 
 member2 = Member.new({
   "first_name" => "Steve",
   "last_name" => "Austin"
   })
+member2.save()
 
 binding.pry
 nil
