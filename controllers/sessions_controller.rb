3a82c45b16a754eb771a49bfa99bf1d9cff20bff
Dotsworthy <dotsworthy@gmail.com>
2020-02-08 13:52:30 +0000

    added save functionality for sessions_controller.rb

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -8,6 +8,16 @@ get "/sessions" do
   erb(:"sessions/index")
 end
 
+post "/sessions" do
+  session = Session.new(params)
+  session.save()
+  redirect to("/sessions")
+end
+
+get "/sessions/new" do
+  erb (:"sessions/new")
+end
+
 get "/sessions/:id" do
   @session = Session.find(params[:id].to_i)
   erb (:"sessions/show")
