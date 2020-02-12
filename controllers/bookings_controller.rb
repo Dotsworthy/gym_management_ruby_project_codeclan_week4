3ac0818f212e1c51a5c124a99f90c40cbce803a8
Dotsworthy <dotsworthy@gmail.com>
2020-02-12 11:11:46 +0000

    cleaned up css

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -1,6 +1,9 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/booking.rb' )
+require_relative('../models/member.rb')
+require_relative('../models/session.rb')
+require_relative('../models/trainer.rb')
 also_reload( '../models/*' )
 
 get "/bookings" do
