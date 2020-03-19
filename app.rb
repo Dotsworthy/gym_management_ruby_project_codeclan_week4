027b02a663d94e3770ed188637625cf09525ab2c
Dotsworthy <dotsworthy@gmail.com>
2020-03-19 11:43:00 +0000

    fixed typo

--- app.rb
+++ app.rb
@@ -1,18 +1,12 @@
 require("sinatra")
 require("sinatra/contrib/all")
-require('Date')
+require('date')
 require_relative("controllers/bookings_controller")
 require_relative("controllers/members_controller")
 require_relative("controllers/sessions_controller")
 require_relative("controllers/trainers_controller")
 require_relative("models/calendar.rb")
 also_reload( '../models/*' )
-
-require("calendar_helper")
-
-helpers do
-  include CalendarHelper
-end
 
 
 get "/" do
